7b63c7b2f9103be1d34d307b2e04848a3c3bd7ef
Richard Jung <Richard@Richards-MacBook-Pro-4.local>
2017-04-11 13:12:44 -0700

    initial commit

---
+++ A0/MyFirstCS499App/app/src/main/java/com/myfirstcs499app/MainActivity.java
@@ -0,0 +1,44 @@
+package com.myfirstcs499app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button trueButton = (Button) findViewById(R.id.trueButton);
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onResume();
+    }
+
+}
